0461b16019ac2c03e8f6e646b04d659b037b6cbc
Chris Baker <cgbaker@hashicorp.com>
2019-05-14 21:10:37 +0000

    updated Makefile and start-nomad scripts to handle Nomad ACL token

--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -7,17 +7,19 @@ if [ ! -e /tmp/vault-test.pid ]; then
     vault server -dev -dev-root-token-id=$VAULT_TEST_TOKEN > /dev/null 2>&1 &
 
     VAULT_PID=$!
-    echo "Started Vault ($VAULT_PID) on $VAULT_ADDR..."
     echo $VAULT_PID > /tmp/vault-test.pid
 fi
 
 if [ ! -e /tmp/nomad-test.pid ]; then
     nomad agent -dev -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false > /dev/null 2>&1 &
     NOMAD_PID=$!
-    echo "Started Nomad ($NOMAD_PID)..."
     echo $NOMAD_PID > /tmp/nomad-test.pid
 
     # Give some time for the process to initialize
     sleep 5
-fi
 
+    http --ignore-stdin POST http://localhost:4646/v1/acl/bootstrap | jq -r '.SecretID' > /tmp/nomad-test.token
+    cat /tmp/nomad-test.token
+elif [ -e /tmp/nomad-test.token ]; then 
+  cat /tmp/nomad-test.token
+fi
