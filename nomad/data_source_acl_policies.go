9336e4a7ca75d891b17762700bf1105d85af8350
anGie44 <angelinepinilla@gmail.com>
2020-06-14 22:33:15 -0400

    add acl_policies data source

---
+++ nomad/data_source_acl_policies.go
@@ -0,0 +1,94 @@
+package nomad
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/hashicorp/nomad/api"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func dataSourceAclPolicies() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceAclPoliciesRead,
+
+		Schema: map[string]*schema.Schema{
+			"prefix": {
+				Description: "ACL Policy Name Prefix",
+				Type:        schema.TypeString,
+				Optional:    true,
+			},
+			"policies": {
+				Description: "ACL Policies",
+				Type:        schema.TypeList,
+				Computed:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Description: "ACL Policy Name",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+						"description": {
+							Description: "ACL Policy Description",
+							Type:        schema.TypeString,
+							Computed:    true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceAclPoliciesRead(d *schema.ResourceData, meta interface{}) error {
+	providerConfig := meta.(ProviderConfig)
+	client := providerConfig.client
+	queryOpts := &api.QueryOptions{}
+	if v, ok := d.GetOk("prefix"); ok && v.(string) != "" {
+		queryOpts.Prefix = v.(string)
+	}
+
+	debugMsg := "[DEBUG] Getting ACL Policies"
+	if queryOpts.Prefix != "" {
+		debugMsg += fmt.Sprintf(" for prefix: %s", queryOpts.Prefix)
+	}
+	log.Printf(debugMsg)
+
+	policies, _, err := client.ACLPolicies().List(queryOpts)
+	if err != nil {
+		// As of Nomad 0.4.1, the API client returns an error for 404
+		// rather than a nil result, so we must check this way.
+		if strings.Contains(err.Error(), "404") {
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("error getting ACL policies: %#v", err)
+	}
+
+	if len(policies) == 0 {
+		return errors.New("query returned an empty list of ACL policies")
+	}
+
+	d.SetId(time.Now().UTC().String())
+	if err := d.Set("policies", flattenAclPolicies(policies)); err != nil {
+		return fmt.Errorf("error setting policies: %#v", err)
+	}
+
+	return nil
+}
+
+func flattenAclPolicies(policies []*api.ACLPolicyListStub) []interface{} {
+	output := make([]interface{}, 0)
+	for _, policy := range policies {
+		p := map[string]interface{}{
+			"name":        policy.Name,
+			"description": policy.Description,
+		}
+		output = append(output, p)
+	}
+	return output
+}
---
+++ nomad/data_source_acl_policies_test.go
@@ -0,0 +1,68 @@
+package nomad
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/nomad/api"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccDataSourceNomadAclPolicies_Basic(t *testing.T) {
+	dataSourceName := "data.nomad_acl_policies.test"
+	numPolicies := 2
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testProviders,
+		Steps: []resource.TestStep{
+			{
+				PreConfig:   testAccCreateNomadAclPolicies(t, numPolicies),
+				Config:      testAccNomadAclPoliciesConfig("non-existent"),
+				ExpectError: regexp.MustCompile(`query returned an empty list of ACL policies`),
+			},
+			{
+				Config: testAccNomadAclPoliciesConfig("tf-acc-test"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "policies.#", "2"),
+					resource.TestMatchResourceAttr(dataSourceName, "policies.0.name", regexp.MustCompile("tf-acc-test")),
+					resource.TestMatchResourceAttr(dataSourceName, "policies.0.description", regexp.MustCompile("Test ACL Policy tf-acc-test")),
+					resource.TestMatchResourceAttr(dataSourceName, "policies.1.name", regexp.MustCompile("tf-acc-test")),
+					resource.TestMatchResourceAttr(dataSourceName, "policies.1.description", regexp.MustCompile("Test ACL Policy tf-acc-test")),
+				),
+			},
+		},
+	})
+}
+
+func testAccNomadAclPoliciesConfig(prefix string) string {
+	return fmt.Sprintf(`
+data "nomad_acl_policies" "test" {
+	prefix = "%s"
+}
+`, prefix)
+}
+
+func testAccCreateNomadAclPolicies(t *testing.T, n int) func() {
+	return func() {
+		client := testProvider.Meta().(ProviderConfig).client
+		for i := 0; i < n; i++ {
+			rName := acctest.RandomWithPrefix("tf-acc-test")
+			policy := api.ACLPolicy{
+				Name:        rName,
+				Description: fmt.Sprintf("Test ACL Policy %s", rName),
+				Rules: `
+				namespace "default" {
+				  policy = "write"
+				}
+				`,
+			}
+			_, err := client.ACLPolicies().Upsert(&policy, nil)
+			if err != nil {
+				t.Fatalf("error inserting ACLPolicy %q: %s", policy.Name, err.Error())
+			}
+		}
+	}
+}
