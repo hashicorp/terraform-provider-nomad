e88e377f95bada32d627baa97ce5fb5d4746b525
Luiz Aoqui <luiz@hashicorp.com>
2021-01-21 10:54:43 -0500

    ci: update hashistack deps

--- scripts/getvault.sh
+++ scripts/getvault.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VAULT_BINARY=https://releases.hashicorp.com/vault/1.5.0+ent/vault_1.5.0+ent_linux_amd64.zip
+VAULT_BINARY=https://releases.hashicorp.com/vault/1.6.1+ent/vault_1.6.1+ent_linux_amd64.zip
 
 curl -L $VAULT_BINARY > vault.zip
 sudo unzip -o vault.zip -d /usr/local/bin
