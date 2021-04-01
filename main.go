5383a32fab05554eae22d96d0eb7edf920b45d1a
Chris Baker <1675087+cgbaker@users.noreply.github.com>
2021-04-01 14:13:01 +0000

    fixed main.go and ran go mod tidy

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+
 	"github.com/hashicorp/terraform-provider-nomad/nomad"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: nomad.Provider})
+		ProviderFunc: func() terraform.ResourceProvider { return nomad.Provider() },
+	})
 }
