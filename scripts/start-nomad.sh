3b3e46183a7f4dcac511f7705360aae13ed8d208
Michele <mdeggies@gmail.com>
2021-05-27 17:46:12 -0700

    See if nomad's starting up

--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -20,7 +20,7 @@ if [ ! -e /tmp/consul-test.pid ]; then
 fi
 
 if [ ! -e /tmp/nomad-test.pid ]; then
-    sudo nomad agent -dev -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false > /tmp/nomad.log 2>&1 &
+    sudo nomad agent -dev -acl-enabled -vault-address=$VAULT_ADDR -vault-token $VAULT_TEST_TOKEN -vault-enabled -vault-allow-unauthenticated=false
     NOMAD_PID=$!
     echo $NOMAD_PID > /tmp/nomad-test.pid
 
