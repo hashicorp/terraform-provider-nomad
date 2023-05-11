fd235825bf38d3c07697ee6a5be010e61ffbc9ec
Ian Fijolek <ian@iamthefij.com>
2023-05-11 12:26:52 -0700

    Fix make localtestacc

--- GNUmakefile
+++ GNUmakefile
@@ -16,10 +16,13 @@ test: fmtcheck
 testacc: fmtcheck
 	TF_ACC=1 go test $(TEST) -v $(TESTARGS) -timeout 120m -count=1
 
-localtestacc: fmtcheck
-	-export NOMAD_TOKEN=$(shell scripts/start-nomad.sh); \
-	export TF_ACC=1; \
-	go test $(TEST) -v $(TESTARGS) -timeout 120m -count=1
+localtestacc-start-nomad:
+	scripts/start-nomad.sh
+
+localtestacc: fmtcheck localtestacc-start-nomad
+	-env NOMAD_TOKEN=$(shell cat /tmp/nomad-test.token) \
+		TF_ACC=1 \
+		go test $(TEST) -v $(TESTARGS) -timeout 120m -count=1
 	scripts/stop-nomad.sh
 
 vet:
--- scripts/start-nomad.sh
+++ scripts/start-nomad.sh
@@ -2,7 +2,6 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-
 set -e
 
 export VAULT_TEST_TOKEN=terraform-provider-nomad-token
@@ -33,15 +32,14 @@ plugin "docker" {
 }
 EOF
 
-    sudo -Eb nomad agent -dev -acl-enabled \
+    sudo -Eb bash -c 'nomad agent -dev -acl-enabled \
       -data-dir=/tmp/nomad/data \
       -config=/tmp/nomad-config.hcl \
       -vault-address=$VAULT_ADDR \
       -vault-token=$VAULT_TEST_TOKEN \
       -vault-enabled \
-      -vault-allow-unauthenticated=false
-    NOMAD_PID=$!
-    echo $NOMAD_PID > /tmp/nomad-test.pid
+      -vault-allow-unauthenticated=false & \
+      echo $! > /tmp/nomad-test.pid'
 
     # Give some time for the process to initialize
     sleep 10
@@ -50,27 +48,37 @@ EOF
     while [ $retries -ge 0 ]; do
       nomad acl bootstrap -json | jq -r '.SecretID' > /tmp/nomad-test.token && break
       sleep 5
-      retries=$(( $retries - 1 ))
+      retries=$(( retries - 1 ))
     done
-    export NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
+    NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
+    export NOMAD_TOKEN
     if [ -z "$NOMAD_TOKEN" ]; then
-      echo "Failed to bootstrap Nomad ACL"
+      echo "Failed to bootstrap Nomad ACL" 1>&2
       exit 1
     fi
-    echo "NOMAD_TOKEN=$(echo $NOMAD_TOKEN)" >> $GITHUB_ENV
+
+    if [ -z "$GITHUB_ENV" ]; then
+      echo "$NOMAD_TOKEN"
+    else
+      echo "NOMAD_TOKEN=$NOMAD_TOKEN" >> "$GITHUB_ENV"
+    fi
 
     # Run hostpath CSI plugin and wait for it to be healthy.
-    nomad job run https://raw.githubusercontent.com/hashicorp/nomad/v1.3.1/demo/csi/hostpath/plugin.nomad
-    echo "Waiting for hostpath CSI plugin to become healthy"
+    nomad job run https://raw.githubusercontent.com/hashicorp/nomad/v1.3.1/demo/csi/hostpath/plugin.nomad 1>&2
+    echo "Waiting for hostpath CSI plugin to become healthy" 1>&2
     retries=30
     while [ $retries -ge 0 ]; do
         nomad plugin status hostpath \
-            | grep "Nodes Healthy        = 1" && break
+            | grep -q "Nodes Healthy        = 1" && break
         sleep 2
-        retries=$(( $retries - 1 ))
+        retries=$(( retries - 1 ))
     done
-    nomad plugin status hostpath
+    nomad plugin status hostpath 1>&2
 elif [ -e /tmp/nomad-test.token ]; then
-  NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
-  echo "NOMAD_TOKEN=$(echo $NOMAD_TOKEN)" >> $GITHUB_ENV
+    NOMAD_TOKEN=$(cat /tmp/nomad-test.token)
+    if [ -z "$GITHUB_ENV" ]; then
+      echo "$NOMAD_TOKEN"
+    else
+      echo "NOMAD_TOKEN=$NOMAD_TOKEN" >> "$GITHUB_ENV"
+    fi
 fi
