33af48a9ad21a9cb7f5d91a93dfed53d589fcff5
Conor Mongey <conor@mongey.net>
2026-02-02 12:20:16 +0000

    update start-nomad script

--- nomad/resource_acl_bootstrap.go
+++ nomad/resource_acl_bootstrap.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -17,7 +18,7 @@ func resourceACLBootstrap() *schema.Resource {
 		Create: resourceACLBootstrapCreate,
 		Delete: resourceACLBootstrapDelete,
 		Read:   resourceACLBootstrapRead,
-		Exists: resourceACLBootstrapExists,
+		// Exists: resourceACLBootstrapExists,
 
 		Schema: map[string]*schema.Schema{
 			"accessor_id": {
@@ -32,6 +33,33 @@ func resourceACLBootstrap() *schema.Resource {
 				Sensitive:   true,
 				Type:        schema.TypeString,
 			},
+			"type": {
+				Description: "The type of token. This will always be 'management' for this resource.",
+				Computed:    true,
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
+			"roles": {
+				Description: "The roles associated with this token.",
+				Computed:    true,
+				Optional:    true,
+				Type:        schema.TypeSet,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+			},
+			"global": {
+				Description: "Whether this token is global.",
+				Computed:    true,
+				Optional:    true,
+				Type:        schema.TypeBool,
+			},
+			"create_time": {
+				Description: "The time this token was created in RFC3339 formate",
+				Computed:    true,
+				Optional:    true,
+				Type:        schema.TypeString,
+			},
 		},
 	}
 }
@@ -40,11 +68,12 @@ func resourceACLBootstrapCreate(d *schema.ResourceData, meta interface{}) error 
 	providerConfig := meta.(ProviderConfig)
 	client := providerConfig.client
 
-	token := api.BootstrapRequest{
-		BootstrapSecret: d.Get("bootstrap_token").(string),
+	token := api.BootstrapRequest{}
+	if v, ok := d.GetOk("bootstrap_token"); ok {
+		token.BootstrapSecret = v.(string)
 	}
 	// create our token
-	log.Println("[DEBUG] Creating ACL Bootstrap token")
+	log.Println("[DEBUG] Creating ACL Bootstrap token with %v", token.BootstrapSecret)
 	resp, _, err := client.ACLTokens().BootstrapOpts(token.BootstrapSecret, nil)
 	if err != nil {
 		// Check if bootstrap was already done
@@ -54,9 +83,17 @@ func resourceACLBootstrapCreate(d *schema.ResourceData, meta interface{}) error 
 		return fmt.Errorf("error bootstrapping the cluster: %w", err)
 	}
 	log.Printf("[DEBUG] Created ACL token AccessorID %q", resp.AccessorID)
+	log.Printf("[DEBUG] Created ACL token %q", resp)
 	d.SetId(resp.AccessorID)
+	d.Set("accessor_id", resp.AccessorID)
+	d.Set("bootstrap_token", resp.SecretID)
+	d.Set("name", resp.Name)
+	d.Set("type", resp.Type)
+	d.Set("roles", resp.Roles)
+	d.Set("global", resp.Global)
+	d.Set("create_time", resp.CreateTime.Format(time.RFC3339))
 
-	return resourceACLBootstrapRead(d, meta)
+	return nil
 }
 
 // not implemented as a cluster bootstrap can't be reverted
@@ -65,41 +102,35 @@ func resourceACLBootstrapDelete(d *schema.ResourceData, meta interface{}) error 
 }
 
 func resourceACLBootstrapRead(d *schema.ResourceData, meta interface{}) error {
-	providerConfig := meta.(ProviderConfig)
-	client := providerConfig.client
-	accessor := d.Id()
+	// providerConfig := meta.(ProviderConfig)
+	// client := providerConfig.client
+	// accessor := d.Id()
 
-	// retrieve the token
-	log.Printf("[DEBUG] Reading ACL bootstrap token %q", accessor)
-	token, _, err := client.ACLTokens().Info(accessor, nil)
-	if err != nil {
-		// we have Exists, so no need to handle 404
-		return fmt.Errorf("error reading ACL token %q: %s", accessor, err.Error())
-	}
-	log.Printf("[DEBUG] Read ACL bootstrap token %q", accessor)
-
-	d.Set("accessor_id", token.AccessorID)
-	d.Set("bootstrap_token", token.SecretID)
+	// log.Printf("[DEBUG] Reading ACL bootstrap token %q", accessor)
+	// token, _, err := client.ACLTokens().Info(accessor, nil)
+	// if err != nil {
+	// 	if strings.Contains(err.Error(), "404") {
+	// 	}
+	// 	return fmt.Errorf("error reading ACL token %q: %s", accessor, err.Error())
+	// }
+	// log.Printf("[DEBUG] Read ACL bootstrap token %q", accessor)
+	//
+	// d.Set("accessor_id", token.AccessorID)
+	// d.Set("bootstrap_token", token.SecretID)
 
 	return nil
 }
 
-func resourceACLBootstrapExists(d *schema.ResourceData, meta interface{}) (bool, error) {
-	providerConfig := meta.(ProviderConfig)
-	client := providerConfig.client
-
-	accessor := d.Id()
-	log.Printf("[DEBUG] Checking if ACL token %q exists", accessor)
-	_, _, err := client.ACLTokens().Info(accessor, nil)
-	if err != nil {
-		// As of Nomad 0.4.1, the API client returns an error for 404
-		// rather than a nil result, so we must check this way.
-		if strings.Contains(err.Error(), "404") {
-			return false, nil
-		}
-
-		return true, fmt.Errorf("error checking for ACL token %q: %#v", accessor, err)
-	}
-
-	return true, nil
-}
+// func resourceACLBootstrapExists(d *schema.ResourceData, meta interface{}) (bool, error) {
+// 	providerConfig := meta.(ProviderConfig)
+// 	client := providerConfig.client
+//
+// 	accessor := d.Id()
+// 	log.Printf("[DEBUG] Checking if ACL token %q exists", accessor)
+// 	_, _, err := client.ACLTokens().Info(accessor, nil)
+// 	if err != nil {
+// 		return true, fmt.Errorf("error checking for ACL token %q: %#v", accessor, err)
+// 	}
+//
+// 	return true, nil
+// }
