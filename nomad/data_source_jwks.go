84b9328572d80fbe2ad1adcfee99124472d38138
Vishnu U <vishnu.u@ibm.com>
2026-03-09 17:34:00 +0530

    feat(data_source_jwks): add EdDSA (Ed25519) key support
    Extend JWKS data source to handle OKP key type in addition to RSA.
    Add 'curve' and 'x' fields to schema for EdDSA key parameters.

--- nomad/data_source_jwks.go
+++ nomad/data_source_jwks.go
@@ -4,13 +4,13 @@
 package nomad
 
 import (
-	"fmt"
-	"log"
-
+	"crypto/ed25519"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
+	"fmt"
+	"log"
 	"math/big"
 
 	"github.com/hashicorp/nomad/api"
@@ -52,6 +52,14 @@ func dataSourceJWKS() *schema.Resource {
 							Type:     schema.TypeString,
 							Computed: true,
 						},
+						"curve": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"x": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
 					},
 				},
 			},
@@ -70,8 +78,10 @@ type Key struct {
 	KeyType   string `json:"kty"`
 	KeyId     string `json:"kid"`
 	Algorithm string `json:"alg"`
-	Modulus   string `json:"n"`
-	Exponent  string `json:"e"`
+	Modulus   string `json:"n"`   // RSA
+	Exponent  string `json:"e"`   // RSA
+	Curve     string `json:"crv"` // EdDSA
+	X         string `json:"x"`   // EdDSA public key
 }
 
 func dataSourceJWKSRead(d *schema.ResourceData, meta any) error {
@@ -117,33 +127,45 @@ func dataSourceJWKSRead(d *schema.ResourceData, meta any) error {
 }
 
 func keyToPem(key Key) (string, error) {
+	var x509Cert []byte
+	var err error
 
-	// Nomad also supports EdDSA keys, but they are not used for OIDC so only
-	// RSA keys should be listed in this endpoint.
-	if key.KeyType != "RSA" {
-		return "", fmt.Errorf("Key type not supported: %s", key.Algorithm)
+	switch key.KeyType {
+	case "RSA":
+		modulus, decErr := base64.RawURLEncoding.DecodeString(key.Modulus)
+		if decErr != nil {
+			return "", fmt.Errorf("Could not decode modulus as base64 from JWK: %s", decErr)
+		}
+		exponent, decErr := base64.RawURLEncoding.DecodeString(key.Exponent)
+		if decErr != nil {
+			return "", fmt.Errorf("Could not decode exponent as base64 from JWK: %s", decErr)
+		}
+
+		modulusInt := new(big.Int)
+		modulusInt.SetBytes(modulus)
+
+		exponentInt := new(big.Int)
+		exponentInt.SetBytes(exponent)
+
+		publicKey := rsa.PublicKey{N: modulusInt, E: int(exponentInt.Uint64())}
+		x509Cert, err = x509.MarshalPKIXPublicKey(&publicKey)
+
+	case "OKP":
+		if key.Curve != "Ed25519" {
+			return "", fmt.Errorf("Unsupported curve: %s", key.Curve)
+		}
+		pubKeyBytes, decErr := base64.RawURLEncoding.DecodeString(key.X)
+		if decErr != nil {
+			return "", fmt.Errorf("Could not decode public key as base64 from JWK: %s", decErr)
+		}
+		if len(pubKeyBytes) != ed25519.PublicKeySize {
+			return "", fmt.Errorf("Invalid Ed25519 key size: got %d, want %d", len(pubKeyBytes), ed25519.PublicKeySize)
+		}
+		x509Cert, err = x509.MarshalPKIXPublicKey(ed25519.PublicKey(pubKeyBytes))
+
+	default:
+		return "", fmt.Errorf("Unsupported key type: %s", key.KeyType)
 	}
-	modulus, err := base64.RawURLEncoding.DecodeString(key.Modulus)
-
-	if err != nil {
-		return "", fmt.Errorf("Could not decode modulus as base64 from JWK: %s", err)
-	}
-
-	exponent, err := base64.RawURLEncoding.DecodeString(key.Exponent)
-
-	if err != nil {
-		return "", fmt.Errorf("Could not decode exponent as base64 from JWK: %s", err)
-	}
-
-	modulusInt := new(big.Int)
-	modulusInt.SetBytes(modulus)
-
-	exponentInt := new(big.Int)
-	exponentInt.SetBytes(exponent)
-
-	publicKey := rsa.PublicKey{N: modulusInt, E: int(exponentInt.Uint64())}
-
-	x509Cert, err := x509.MarshalPKIXPublicKey(&publicKey)
 
 	if err != nil {
 		return "", fmt.Errorf("Could not marshal JWK public key to X509 PKIX: %s", err)
@@ -168,6 +190,8 @@ func fromKeys(keys []Key) []interface{} {
 			"algorithm": key.Algorithm,
 			"modulus":   key.Modulus,
 			"exponent":  key.Exponent,
+			"curve":     key.Curve,
+			"x":         key.X,
 		}
 		output = append(output, p)
 	}
