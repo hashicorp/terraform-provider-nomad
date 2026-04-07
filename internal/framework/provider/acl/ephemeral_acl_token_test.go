b3ac78f0974531d014a74c377d523c04c6693dad
Vishnu U <vishnu.u@ibm.com>
2026-04-07 12:07:55 +0530

    move test helper functions to testutil

--- internal/framework/provider/acl/ephemeral_acl_token_test.go
+++ internal/framework/provider/acl/ephemeral_acl_token_test.go
@@ -3,23 +3,17 @@
 package acl_test
 
 import (
-	"context"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/nomad/api"
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	sdkv2 "github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
-	"github.com/hashicorp/terraform-provider-nomad/internal/framework/provider"
+	"github.com/hashicorp/terraform-provider-nomad/internal/framework/provider/testutil"
 	"github.com/hashicorp/terraform-provider-nomad/nomad"
 )
 
@@ -27,8 +21,8 @@ func TestAccEphemeralACLToken_basic(t *testing.T) {
 	accessorID, tokenName := createTestACLToken(t)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories(),
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories(),
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_10_0),
 		},
@@ -86,7 +80,7 @@ resource "echo" "test" {}
 func createTestACLToken(t *testing.T) (string, string) {
 	t.Helper()
 
-	providerData := sdkv2providerMeta(t)()
+	providerData := testutil.SDKV2ProviderMeta(t)()
 	providerConfig, ok := providerData.(nomad.ProviderConfig)
 	if !ok {
 		t.Fatalf("expected nomad.ProviderConfig, got %T", providerData)
@@ -115,43 +109,4 @@ func createTestACLToken(t *testing.T) (string, string) {
 	})
 
 	return token.AccessorID, tokenName
-}
-
-func sdkv2providerMeta(t *testing.T) func() any {
-	t.Helper()
-
-	p := nomad.Provider()
-	if err := p.Configure(context.Background(), sdkv2.NewResourceConfigRaw(nil)); err != nil {
-		t.Fatalf("failed to configure sdkv2 provider: %v", err)
-	}
-
-	return p.Meta
-}
-
-func testAccProtoV6ProviderFactories() map[string]func() (tfprotov6.ProviderServer, error) {
-	return map[string]func() (tfprotov6.ProviderServer, error){
-		"nomad": func() (tfprotov6.ProviderServer, error) {
-			return providerserver.NewProtocol6WithError(provider.New(sdkv2providerMetaForFactory()))()
-		},
-		"echo": echoprovider.NewProviderServer(),
-	}
-}
-
-func sdkv2providerMetaForFactory() func() any {
-	p := nomad.Provider()
-	if err := p.Configure(context.Background(), sdkv2.NewResourceConfigRaw(nil)); err != nil {
-		panic(fmt.Sprintf("failed to configure sdkv2 provider: %v", err))
-	}
-
-	return p.Meta
-}
-
-func testAccPreCheck(t *testing.T) {
-	t.Helper()
-
-	if os.Getenv("NOMAD_ADDR") == "" {
-		os.Setenv("NOMAD_ADDR", "http://127.0.0.1:4646")
-	}
-
-	_ = sdkv2providerMeta(t)
 }
