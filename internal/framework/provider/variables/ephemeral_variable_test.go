14b5e9d2fc893f545256941b2d61388a31f2e511
Vishnu U <vishnu.u@ibm.com>
2026-04-09 21:39:25 +0530

    use methods from testutils instead of redefining

--- internal/framework/provider/variables/ephemeral_variable_test.go
+++ internal/framework/provider/variables/ephemeral_variable_test.go
@@ -3,23 +3,17 @@
 package variables_test
 
 import (
-	"context"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/nomad/api"
-	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	sdkv2 "github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
-	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
 	"github.com/hashicorp/terraform-plugin-testing/statecheck"
 	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
 	"github.com/hashicorp/terraform-plugin-testing/tfversion"
-	"github.com/hashicorp/terraform-provider-nomad/internal/framework/provider"
+	"github.com/hashicorp/terraform-provider-nomad/internal/framework/provider/testutil"
 	"github.com/hashicorp/terraform-provider-nomad/nomad"
 )
 
@@ -28,8 +22,8 @@ func TestAccEphemeralVariable_basic(t *testing.T) {
 	createTestVariable(t, path, api.DefaultNamespace)
 
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories(),
+		PreCheck:                 func() { testutil.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: testutil.TestAccProtoV6ProviderFactories(),
 		TerraformVersionChecks: []tfversion.TerraformVersionCheck{
 			tfversion.SkipBelow(tfversion.Version1_10_0),
 		},
@@ -78,7 +72,7 @@ resource "echo" "test" {}
 func createTestVariable(t *testing.T, path, namespace string) {
 	t.Helper()
 
-	providerData := sdkv2providerMeta(t)()
+	providerData := testutil.SDKV2ProviderMeta(t)()
 	providerConfig, ok := providerData.(nomad.ProviderConfig)
 	if !ok {
 		t.Fatalf("expected nomad.ProviderConfig, got %T", providerData)
@@ -101,43 +95,4 @@ func createTestVariable(t *testing.T, path, namespace string) {
 			t.Logf("failed to delete test variable %q@%q: %v", path, namespace, err)
 		}
 	})
-}
-
-func sdkv2providerMeta(t *testing.T) func() any {
-	t.Helper()
-
-	p := nomad.Provider()
-	if err := p.Configure(context.Background(), sdkv2.NewResourceConfigRaw(nil)); err != nil {
-		t.Fatalf("failed to configure sdkv2 provider: %v", err)
-	}
-
-	return p.Meta
-}
-
-func testAccProtoV6ProviderFactories() map[string]func() (tfprotov6.ProviderServer, error) {
-	return map[string]func() (tfprotov6.ProviderServer, error){
-		"nomad": func() (tfprotov6.ProviderServer, error) {
-			return providerserver.NewProtocol6WithError(provider.New(sdkv2providerMetaForFactory()))()
-		},
-		"echo": echoprovider.NewProviderServer(),
-	}
-}
-
-func sdkv2providerMetaForFactory() func() any {
-	p := nomad.Provider()
-	if err := p.Configure(context.Background(), sdkv2.NewResourceConfigRaw(nil)); err != nil {
-		panic(fmt.Sprintf("failed to configure sdkv2 provider: %v", err))
-	}
-
-	return p.Meta
-}
-
-func testAccPreCheck(t *testing.T) {
-	t.Helper()
-
-	if os.Getenv("NOMAD_ADDR") == "" {
-		os.Setenv("NOMAD_ADDR", "http://127.0.0.1:4646")
-	}
-
-	_ = sdkv2providerMeta(t)
 }
