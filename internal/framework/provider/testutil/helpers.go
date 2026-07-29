d7037210a60a73be9a62cd2581c3ba8115987946
Vishnu U <vishnu.u@ibm.com>
2026-07-29 13:49:36 +0530

    test: migrate existing nomad_job resource tests and add out of band and sidecar testcases

--- internal/framework/provider/testutil/helpers.go
+++ internal/framework/provider/testutil/helpers.go
@@ -7,6 +7,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/go-version"
+	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
 	sdkv2 "github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
@@ -56,5 +58,71 @@ func sdkv2ProviderMetaForFactory(t *testing.T) func() any {
 func ensureNomadAddrEnv() {
 	if os.Getenv("NOMAD_ADDR") == "" {
 		os.Setenv("NOMAD_ADDR", "http://127.0.0.1:4646")
+	}
+}
+
+// NomadClient returns a Nomad API client using the configured SDKv2 provider meta.
+func NomadClient(t *testing.T) *api.Client {
+	t.Helper()
+	return SDKV2ProviderMeta(t)().(nomad.ProviderConfig).Client()
+}
+
+// CheckMinVersion skips the test if the first Nomad node's version is older than min.
+func CheckMinVersion(t *testing.T, min string) {
+	t.Helper()
+	minVer, err := version.NewVersion(min)
+	if err != nil {
+		t.Skipf("failed to parse min version %q: %v", min, err)
+	}
+	client := NomadClient(t)
+	nodes, _, err := client.Nodes().List(nil)
+	if err != nil || len(nodes) == 0 {
+		t.Skip("could not list nodes to check version")
+	}
+	nodeVer, err := version.NewVersion(nodes[0].Version)
+	if err != nil {
+		t.Skip("could not parse node version: ", err)
+	}
+	if !nodeVer.Core().GreaterThanOrEqual(minVer) {
+		t.Skipf("node version %q is older than minimum %q", nodeVer, minVer)
+	}
+}
+
+// CheckEnt skips the test if the Nomad build is not enterprise.
+func CheckEnt(t *testing.T) {
+	t.Helper()
+	client := NomadClient(t)
+	nodes, _, err := client.Nodes().List(nil)
+	if err != nil || len(nodes) == 0 {
+		t.Skip("could not list nodes to check enterprise")
+	}
+	nodeVer, err := version.NewVersion(nodes[0].Version)
+	if err != nil {
+		t.Skip("could not parse node version: ", err)
+	}
+	if nodeVer.Metadata() != "ent" {
+		t.Skipf("node version %q is not an enterprise build", nodeVer)
+	}
+}
+
+// CheckEntFeatures skips the test if the Nomad enterprise license does not include all required features.
+func CheckEntFeatures(t *testing.T, requiredFeatures ...string) {
+	t.Helper()
+	CheckEnt(t)
+	client := NomadClient(t)
+	resp, _, err := client.Operator().LicenseGet(nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	licensed := map[string]bool{}
+	if resp != nil && resp.License != nil {
+		for _, f := range resp.License.Features {
+			licensed[f] = true
+		}
+	}
+	for _, f := range requiredFeatures {
+		if !licensed[f] {
+			t.Skipf("license doesn't include required feature %q", f)
+		}
 	}
 }
